6da1c8fd92e642b00b8e844d683a0b35cc3e189c
JPowderly814 <julie.larkin.814@gmail.com>
2021-07-19 20:15:48 -0500

    fixed login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,10 @@
 import './Login.css';
 import Register from './Register';
-import { useState, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { UserContext } from '../..';
 import Axios from 'axios';
 import ErrorModal from '../UI/ErrorModal';
+
 //import { Link, Route } from 'react-router-dom';
 
 function Login() {
@@ -15,70 +16,39 @@ function Login() {
     localStorage.getItem('token') !== 'null'
   );
   const userService = useContext(UserContext);
+
   Axios.defaults.withCredentials = true;
 
-  const handleLogin = async (e) => {};
-
-  const startRegistrationHandler = () => {
-    setIsRegistering(true);
-  };
-
-  const stopRegistrationHandler = () => {
-    setIsRegistering(false);
-  };
-
-  const login = () => {
-    Axios.post('http://localhost:3001/login', {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if (response.data.message) {
-        setLoggedIn(false);
-        setError({
-          title: 'Invalid',
-          message: response.data.message,
-        });
-        return;
-      } else {
-        console.log(response.data.result[0]);
-        // Get rid of local storage
-        localStorage.setItem('token', response.data.token);
-        localStorage.setItem('expiresIn', response.data.token.expires);
-        localStorage.setItem('user', response.data.result[0].id);
-        console.log(localStorage.getItem('user'));
-
-        setLoggedIn(true);
-      }
-    });
-
+  const handleLogin = async () => {
+    let response = await userService.login(username, password);
+    if (response.data.message) {
+      setError({
+        title: 'Invalid',
+        message: response.data.message,
+      });
+    }
+    setLoggedIn(true);
     setUsername('');
     setPassword('');
   };
 
-  const logout = () => {
-    Axios.post('http://localhost:3001/logout', {}).then((response) => {
-      if (response.data.message) {
-        setLoggedIn(true);
+  const startRegistrationHandler = () => {
+    setIsRegistering(true);
+  };
+  const stopRegistrationHandler = () => {
+    setIsRegistering(false);
+  };
 
-        setError({
-          title: 'Invalid',
-          message: response.data.message,
-        });
-        return;
-      } else {
-        console.log();
-        localStorage.setItem('token', null);
-        localStorage.setItem('user', null);
-        console.log(localStorage.getItem('token'));
-        console.log(localStorage.getItem('user'));
-
-        //setLoginStatus(response.data[0].RowDataPacket.username);
-        setLoggedIn(true);
-      }
-    });
+  const handleLogout = async () => {
+    // await userService.logout();
+    let response = await userService.logout();
+    if (response.data.message) {
+      setError({
+        title: 'Invalid',
+        message: response.data.message,
+      });
+    }
     setLoggedIn(false);
-    localStorage.setItem('token', null);
-    localStorage.setItem('user', null);
     setUsername('');
     setPassword('');
   };
@@ -86,7 +56,6 @@ function Login() {
   const errorHandler = () => {
     setError(null);
   };
-
   return (
     <div className="login-register-container">
       {error && (
@@ -99,7 +68,6 @@ function Login() {
       <div className="text-center">
         <h2 className="login-register-header">Sign in</h2>
       </div>
-
       <div className="login-register-form">
         {localStorage.getItem('token') === 'null' && (
           <div className="form-group row">
@@ -131,12 +99,12 @@ function Login() {
         )}
 
         {localStorage.getItem('token') !== 'null' && (
-          <button className="login-btn" onClick={logout}>
+          <button className="login-btn" onClick={handleLogout}>
             logout
           </button>
         )}
         {localStorage.getItem('token') === 'null' && (
-          <button className="login-btn" onClick={login}>
+          <button className="login-btn" onClick={handleLogin}>
             login
           </button>
         )}
@@ -147,11 +115,9 @@ function Login() {
             register
           </button>
         )}
-
         {isRegistering && <Register onCancel={stopRegistrationHandler} />}
       </div>
     </div>
   );
 }
-
 export default Login;
