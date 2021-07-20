9438d12e29608481edf0be879857e06ed3751a0d
JPowderly814 <julie.larkin.814@gmail.com>
2021-07-19 19:40:49 -0500

    I think I fixed the login component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,10 +1,9 @@
 import './Login.css';
 import Register from './Register';
-import { useState, useEffect, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { UserContext } from '../..';
 import Axios from 'axios';
 import ErrorModal from '../UI/ErrorModal';
-
 //import { Link, Route } from 'react-router-dom';
 
 function Login() {
@@ -16,21 +15,9 @@ function Login() {
     localStorage.getItem('token') !== 'null'
   );
   const userService = useContext(UserContext);
-
   Axios.defaults.withCredentials = true;
 
-  const handleLogin = async () => {
-    let response = await userService.login(username, password);
-    if (response.data.message) {
-      setError({
-        title: 'Invalid',
-        message: response.data.message,
-      });
-    }
-    setLoggedIn(true);
-    setUsername('');
-    setPassword('');
-  };
+  const handleLogin = async (e) => {};
 
   const startRegistrationHandler = () => {
     setIsRegistering(true);
@@ -40,15 +27,60 @@ function Login() {
     setIsRegistering(false);
   };
 
-  const handleLogout = async () => {
-    await userService.logout();
-    // let response = await userService.logout();
-    // if (response.data.message) {
-    //   setError({
-    //     title: 'Invalid',
-    //     message: response.data.message,
-    //   });
-    // }
+  const login = () => {
+    Axios.post('http://localhost:3001/login', {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoggedIn(false);
+        setError({
+          title: 'Invalid',
+          message: response.data.message,
+        });
+        return;
+      } else {
+        console.log(response.data.result[0]);
+        // Get rid of local storage
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('expiresIn', response.data.token.expires);
+        localStorage.setItem('user', response.data.result[0].id);
+        console.log(localStorage.getItem('user'));
+
+        setLoggedIn(true);
+      }
+    });
+
+    setUsername('');
+    setPassword('');
+  };
+
+  const logout = () => {
+    Axios.post('http://localhost:3001/logout', {}).then((response) => {
+      if (response.data.message) {
+        setLoggedIn(true);
+
+        setError({
+          title: 'Invalid',
+          message: response.data.message,
+        });
+        return;
+      } else {
+        console.log();
+        localStorage.setItem('token', null);
+        localStorage.setItem('user', null);
+        console.log(localStorage.getItem('token'));
+        console.log(localStorage.getItem('user'));
+
+        //setLoginStatus(response.data[0].RowDataPacket.username);
+        setLoggedIn(true);
+      }
+    });
+    setLoggedIn(false);
+    localStorage.setItem('token', null);
+    localStorage.setItem('user', null);
+    setUsername('');
+    setPassword('');
   };
 
   const errorHandler = () => {
@@ -99,12 +131,12 @@ function Login() {
         )}
 
         {localStorage.getItem('token') !== 'null' && (
-          <button className="login-btn" onClick={handleLogout}>
+          <button className="login-btn" onClick={logout}>
             logout
           </button>
         )}
         {localStorage.getItem('token') === 'null' && (
-          <button className="login-btn" onClick={handleLogin}>
+          <button className="login-btn" onClick={login}>
             login
           </button>
         )}
