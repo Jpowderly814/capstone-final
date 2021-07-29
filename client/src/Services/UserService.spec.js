ea09d7d3f55462618e2d9f9628e9eb1478b59595
JPowderly814 <julie.larkin.814@gmail.com>
2021-07-29 10:23:13 -0500

    Fixed error, working on tests

--- client/src/Services/UserService.spec.js
+++ client/src/Services/UserService.spec.js
@@ -1,21 +1,30 @@
-import UserService from './UserService';
+import UserService, { login } from './UserService';
 // import MockAdapter from 'axios-mock-adapter';
 
 import axios from 'axios';
-jest.mock('axios');
+
+jest.mock('axios', () => {
+  return {
+    post: jest.fn(),
+  };
+});
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe('User Service', () => {
+  const userService = new UserService();
   const username = 'username123';
   const password = 'password123';
 
+  it('should call endpoint', async () => {
+    userService.login();
+    expect(axios.post).toBeCalled();
+  });
+
   it('should call endpoint with given email & password', async () => {
-    await UserService.login(username, password);
-    expect(axios.post).toBeCalledWith('http://localhost:3001/login', {
-      user: { username, password },
-    });
+    userService.login(username, password);
+    expect(axios.post()).toBeCalledWith(username, password);
   });
 });
